35c90023fe04f3aa1a8a0f5169589937fddff3a3
atharv-dusane <atharv2611@gmail.com>
2024-10-02 20:43:44 +0530

    Added Base Utilities

--- JewelleryManagementSystem/App.xaml.cs
+++ JewelleryManagementSystem/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using JewelleryManagementSystem.UIUtilities;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,13 @@ namespace JewelleryManagementSystem
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var desktopWindo = new MainWindow();
+            desktopWindo.Show();
+        }
+
     }
 
 }
---
+++ JewelleryManagementSystem/ModelUtilities/DataManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace JewelleryManagementSystem.ModelUtilities
+{
+    public class DataManager
+    {
+        public static void Save<T>(T serializableObject, string filePath, DataContractSerializerSettings settings = null)
+        {
+            try
+            {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(T), settings);
+
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+                using (XmlWriter writer = XmlWriter.Create(fileStream))
+                {
+                    serializer.WriteObject(writer, serializableObject);
+                }
+            }
+            catch (Exception ex)
+            {
+                CommonActions.OnError?.Invoke("Error in Save");
+            }
+        }
+        public static T Read<T>(string filePath, DataContractSerializerSettings settings = null)
+        {
+            try
+            {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(T), settings);
+
+                using (FileStream fileStream = new FileStream(filePath, FileMode.Open))
+                {
+                    T obj = (T)serializer.ReadObject(fileStream);
+                    Console.WriteLine($"Object deserialized from {filePath}");
+                    return obj;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log error details
+                CommonActions.OnError?.Invoke("Error in Read");
+                return default;
+            }
+        }
+    }
+}
